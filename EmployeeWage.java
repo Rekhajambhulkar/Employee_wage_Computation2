67ecec547420b0b28b62df4ecbb204f8beec4b4a
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2020-09-28 23:09:09 +0530

    Wage_Part_Time

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,9 +5,14 @@ public class EmployeeWage
 public static final int IS_Employee_Present=1;
 public static final int Wage_Per_Hour=20;
 public static final int Full_Time=1;
+public static final Part_Time=0;
 
 public static void main(String args[])
 {
+
+//variables
+int totalHours=0;
+int totalDays=0;
 
 //Print the welcome Message
 System.out.println("Welcome to the EmployeeWage Program");
