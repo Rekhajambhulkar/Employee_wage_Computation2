4da5b1881319d0dffb870e7dc2dc57eebe0b93e7
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2020-09-29 13:46:59 +0530

    Store the result in DailyWage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -10,46 +10,39 @@ public static final int WorkingDay_Per_Month=20;
 
 public static void main(String args[])
 {
-
 //variables
-int totalHours=0;
-int totalDays=0;
-
-//Print the welcome Message
-System.out.println("Welcome to the EmployeeWage Program");
-
-//generate 0-2
-double employeeCheck=Math.floor(Math.random()*10)%3;
-
-// TYPE CASTING - external
-int employeeCheck1 = (int) (Math.floor(Math.random()*10)%3);
-System.out.println("integer generated - "+employeeCheck1);
-
-/*
-* Switch Case
-*/
-switch(employeeCheck1)
-{
-case Full_Time :
-	System.out.println("Employee is full time");
-	totalHours=8;
-	break;
-case Part_Time:
-	System.out.println("Employee is part time");
-	totalHours=4;
-	break;
-default :
-	System.out.println("employee absent");
-	break;
-		}
-
-//Using while Loop Check total working hours or days is reached for a month
-while(totalHours <  Max_Hrs_In_Month && totalDays < WorkingDay_Per_Month)
+	int totalHours=0;
+	int totalDays=0;
+	int EmpHrs=0;
+	while(totalHours <  Max_Hrs_In_Month && totalDays < WorkingDay_Per_Month)
 	{
-		totalHours+=1;
-		totalDays+=1;
-		int DailyWage=totalHours*totalDays*Wage_Per_Hour;
-		System.out.println("Wage is:"+DailyWage);
+		totalDays++;
+		int employeeCheck1 = (int) (Math.floor(Math.random()*10)%3);
+		System.out.println("integer generated - "+employeeCheck1);
+		/*
+		* Switch Case
+		*/
+		switch(employeeCheck1)
+		{
+		case Full_Time :
+		System.out.println("Employee is full time");
+		totalHours=8;
+		break;
+		case Part_Time:
+		System.out.println("Employee is part time");
+		totalHours=4;
+		break;
+		default :
+		System.out.println("employee absent");
+		break;
+		}
+		totalHours+=EmpHrs;
+		System.out.println("Days:"+totalDays+"Employee hour :"+EmpHrs);
 		
-	}	}
-}
+	}
+	int totalEmployeeWage=totalHours*Max_Hrs_In_Month;
+	System.out.println("Total Employee wage is:"+totalEmployeeWage);
+	}
+	}
+
+
